6ed617009898b7ba38bd0bacd844be2f5cc7edda
hasnain-mirani <hasnainmirani1122@gmail.com>
2026-02-04 21:02:18 +0500

    updated

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,35 +1,35 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+// const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI in .env.local");
-}
+// if (!MONGODB_URI) {
+//   throw new Error("Please define MONGODB_URI in .env.local");
+// }
 
-type MongooseCache = {
-  conn: typeof mongoose | null;
-  promise: Promise<typeof mongoose> | null;
-};
+// type MongooseCache = {
+//   conn: typeof mongoose | null;
+//   promise: Promise<typeof mongoose> | null;
+// };
 
-// eslint-disable-next-line no-var
-declare global {
-  var mongooseCache: MongooseCache | undefined;
-}
+// // eslint-disable-next-line no-var
+// declare global {
+//   var mongooseCache: MongooseCache | undefined;
+// }
 
-const globalCache = global.mongooseCache ?? { conn: null, promise: null };
-global.mongooseCache = globalCache;
+// const globalCache = global.mongooseCache ?? { conn: null, promise: null };
+// global.mongooseCache = globalCache;
 
-export async function dbConnect() {
-  if (globalCache.conn) return globalCache.conn;
+// export async function dbConnect() {
+//   if (globalCache.conn) return globalCache.conn;
 
-  if (!globalCache.promise) {
-    globalCache.promise = mongoose
-      .connect(MONGODB_URI, {
-        bufferCommands: false,
-      })
-      .then((m) => m);
-  }
+//   if (!globalCache.promise) {
+//     globalCache.promise = mongoose
+//       .connect(MONGODB_URI, {
+//         bufferCommands: false,
+//       })
+//       .then((m) => m);
+//   }
 
-  globalCache.conn = await globalCache.promise;
-  return globalCache.conn;
-}
+//   globalCache.conn = await globalCache.promise;
+//   return globalCache.conn;
+// }
