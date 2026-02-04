87f62502633eeeb8887bcb7892602eae39169a45
hasnain-mirani <hasnainmirani1122@gmail.com>
2026-02-04 21:07:54 +0500

    updated

--- lib/adminAuth.ts
+++
@@ -1,8 +0,0 @@
-import { cookies } from "next/headers";
-
-const COOKIE_NAME = "admin_token";
-
-export async function isAuthedOnServer() {
-  const token = (await cookies()).get(COOKIE_NAME)?.value;
-  return Boolean(token && process.env.ADMIN_TOKEN && token === process.env.ADMIN_TOKEN);
-}
